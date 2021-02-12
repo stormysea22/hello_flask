ab968536d80e4ef8d6e60209d51976dc63122070
Becca Theismann <becca@theismannconsulting.com>
2021-02-12 14:42:11 -0500

    initial commit

---
+++ hello.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/<name>/<times>')
+def hello_person(name, times):
+    return render_template('name.html', some_name=name, num_times=int(times))
+
+@app.route('/lists')
+def render_lists():
+    # Soon enough, we'll get data from a database, but for now, we're hard coding data
+    student_info = [
+        {'name' : 'Michael', 'age' : 35},
+        {'name' : 'John', 'age' : 30 },
+        {'name' : 'Mark', 'age' : 25},
+        {'name' : 'KB', 'age' : 27}
+    ]
+    return render_template("lists.html", random_numbers = [3,1,5], students = student_info)
+
+
+if __name__=="__main__":
+    app.run(debug=True)
+
